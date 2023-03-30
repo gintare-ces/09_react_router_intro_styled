bde0a744431544ebca9ce303251f64355713cd0b
gintare-ces <gintare.cesiuliene@gmail.com>
2023-03-30 10:42:21 +0300

    browse and route

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import './styles/App.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="">
-      <HomePage />
-      <AboutPage />
-      <ContactPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/contacts' element={<ContactPage />} />
+      </Routes>
+      
+      
+      
     </div>
   );
 }
